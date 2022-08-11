a4763488fd1de93ca1f3b5936966f2b439c56892
Matheus Fonseca de Souza <matheus.fonseca@CONTABILIZEI.COM.BR>
2022-08-11 17:17:30 -0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Site muito legal</h1>
+  <AlertCard v-if="showAlert" @fecha="fecharAlert" text="seja bem vindo!!!" />
+  <img class="imgGoku" v-if="!showAlert" src="https://static.wikia.nocookie.net/1e7ff1f7-6d62-4d43-9b48-e4bf98ff63f9" alt="gif goku se aquecendo">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlertCard from './components/AlertCard.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      showAlert: true
+    }
+  },
+  methods:{
+    fecharAlert(){
+      this.showAlert = false
+    }
+  },
   components: {
-    HelloWorld
-  }
+    AlertCard
+}
 }
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+h1{
+  text-align: center;
+  margin-top: 15px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif, Helvetica, Arial;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.imgGoku{
+  width: 100%;
 }
 </style>
---
+++ src/components/AlertCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p>{{text}}</p>
+
+    <button type="button" @click="fechaItem">
+        x
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AlertCard',
+    props:{
+        text: String
+    },
+    methods:{
+        fechaItem(){
+            this.$emit('fecha')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div{
+        width: 100%;
+        position: absolute;
+        top: 15px;
+        left: 0;
+        right: 0;
+        background: rgb(202, 69, 255);
+        color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+    }
+    div button{
+        width: 30px;
+        border: 0;
+        background: transparent;
+        color: #fff;
+        text-transform: uppercase;
+        font-size: 26px;
+        cursor: pointer;
+    }
+</style>
